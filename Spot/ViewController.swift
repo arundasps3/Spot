3b0cabfcdff2ffe3853bb2703f382f1f6a88e529
arundasps <arundasps3@gmail.com>
2019-11-27 11:11:08 -0500

    Initial Commit

---
+++ Spot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spot
+//
+//  Created by ARUNDAS PS on 2019-11-27.
+//  Copyright © 2019 ARUNDAS PS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
